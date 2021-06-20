73a3594908e195225d9a7e09d4173e96466c2f19
okirano <teamsinjito.rano@gmail.com>
2021-06-20 17:05:57 +0900

    テーブル表示方法変更とチャート軽微修正

--- pages/details.vue
+++ pages/details.vue
@@ -26,26 +26,28 @@
           </v-select>
         </v-col>
         <v-col>
-          <v-autocomplete
+          <v-select
             v-model="selectClientsAndCosts"
             :items="clientsAndCosts"
+            multiple
+            return-object
             item-value="id"
             item-text="name"
             dense
             class="mt-6 text-caption"
-            @change="changeSubject()"
+            @change="changeClientsAndCosts()"
           >
             <template #label>
               <span class="text-caption">取引先および固定経費を選択</span>
             </template>
-          </v-autocomplete>
+          </v-select>
         </v-col>
       </v-row>
 
       <vue-good-table
         :columns="columns"
         :rows="records"
-        styleClass="vgt-table striped bordered condensed"
+        style-class="vgt-table striped bordered condensed"
         class="text-caption mx-3 w-100"
         style="font-size: 0.8rem"
       />
@@ -66,43 +68,10 @@ export default defineComponent({
   components: { DetailsHeaderBar },
   setup() {
     const useState = useGlobalState()
-    const date = [
-      { id: '1', value: '01' },
-      { id: '2', value: '02' },
-      { id: '3', value: '03' },
-      { id: '4', value: '04' },
-      { id: '5', value: '05' },
-      { id: '6', value: '06' },
-      { id: '7', value: '07' },
-      { id: '8', value: '08' },
-      { id: '9', value: '09' },
-      { id: '10', value: '10' },
-      { id: '11', value: '11' },
-      { id: '12', value: '12' },
-      { id: '13', value: '13' },
-      { id: '14', value: '14' },
-      { id: '15', value: '15' },
-      { id: '16', value: '16' },
-      { id: '17', value: '17' },
-      { id: '18', value: '18' },
-      { id: '19', value: '19' },
-      { id: '20', value: '20' },
-      { id: '21', value: '21' },
-      { id: '22', value: '22' },
-      { id: '23', value: '23' },
-      { id: '24', value: '24' },
-      { id: '25', value: '25' },
-      { id: '26', value: '26' },
-      { id: '27', value: '27' },
-      { id: '28', value: '28' },
-      { id: '29', value: '29' },
-      { id: '30', value: '30' },
-      { id: '31', value: '31' },
-    ]
     const state = reactive<{
       selectSubject: string
       subjectItems: string[]
-      selectClientsAndCosts: string
+      selectClientsAndCosts: { id: string; name: string }[]
       selectClientsAndCostsName: string
       clientsAndCosts: { id: string; name: string }[]
       records: {
@@ -124,189 +93,11 @@ export default defineComponent({
     }>({
       selectSubject: '仕入',
       subjectItems: [],
-      selectClientsAndCosts: '',
+      selectClientsAndCosts: [],
       selectClientsAndCostsName: '',
       clientsAndCosts: [{ id: '', name: '' }],
-      records: [
-        {
-          cNameS: '',
-          jan: '0',
-          feb: '0',
-          mar: '0',
-          apr: '0',
-          may: '0',
-          jun: '0',
-          jul: '0',
-          aug: '0',
-          sep: '0',
-          oct: '0',
-          nov: '0',
-          dec: '0',
-          cNameE: '',
-        },
-      ],
+      records: [],
     })
-    // 勘定科目セット
-    const setSubjects = () => {
-      const items = useState.subjectsInfo.value.reduce((ary: string[], g) => {
-        ary.push(g.groupname)
-        return ary
-      }, [])
-      state.subjectItems = [...new Set(items)]
-      console.log(state.subjectItems)
-    }
-    // 取引先および固定経費セット
-    const setClientsAndCosts = () => {
-      const items = useState.clientsAndCostsInfo.value.map((i) => {
-        return { id: i.id, name: i.name }
-      })
-      state.clientsAndCosts = [...new Set(items)]
-      state.selectClientsAndCosts = items[0].id
-    }
-
-    // データ整形処理
-    const getFilteringData = () => {
-      state.records.length = 0
-      // 選択している勘定科目でフィルタリング
-      const filterRecordsManagements = useState.workRecordsManagement.value.filter(
-        (rm) => {
-          const cl = useState.clientsAndCostsInfo.value
-            .filter((s) => {
-              return s.id === state.selectClientsAndCosts
-            })
-            .map((fs) => {
-              return fs.id
-            })
-          const subjects = useState.subjectsInfo.value
-            .filter((s) => {
-              return s.groupname === state.selectSubject
-            })
-            .map((fs) => {
-              return fs.id
-            })
-
-          return cl.includes(rm.cid) && subjects.includes(rm.sid)
-        }
-      )
-      console.log(filterRecordsManagements)
-      // 取引先または固定経費毎に月毎の金額を算出
-      date.forEach((d) => {
-        // 取引先または固定経費毎にフィルタリング
-        const filterByClientsAndCosts = filterRecordsManagements
-          .filter((r) => {
-            return r.day.slice(8, 10) === d.value
-          })
-          .map((i) => {
-            return {
-              pay: i.pay,
-              month: Number(i.day.slice(5, 7)),
-            }
-          })
-
-        state.records.push({
-          cNameS: d.id + '日',
-          jan: sumPerMonth(filterByClientsAndCosts, 1),
-          feb: sumPerMonth(filterByClientsAndCosts, 2),
-          mar: sumPerMonth(filterByClientsAndCosts, 3),
-          apr: sumPerMonth(filterByClientsAndCosts, 4),
-          may: sumPerMonth(filterByClientsAndCosts, 5),
-          jun: sumPerMonth(filterByClientsAndCosts, 6),
-          jul: sumPerMonth(filterByClientsAndCosts, 7),
-          aug: sumPerMonth(filterByClientsAndCosts, 8),
-          sep: sumPerMonth(filterByClientsAndCosts, 9),
-          oct: sumPerMonth(filterByClientsAndCosts, 10),
-          nov: sumPerMonth(filterByClientsAndCosts, 11),
-          dec: sumPerMonth(filterByClientsAndCosts, 12),
-          cNameE: d.id + '日',
-        })
-      })
-
-      // 合計行の算出
-      state.records.push({
-        cNameS: '合計',
-        jan: state.records
-          .reduce((acc, r): number => {
-            return acc + Number(r.jan.replaceAll(',', ''))
-          }, 0)
-          .toLocaleString(),
-        feb: state.records
-          .reduce((acc, r) => {
-            return acc + Number(r.feb.replaceAll(',', ''))
-          }, 0)
-          .toLocaleString(),
-        mar: state.records
-          .reduce((acc, r) => {
-            return acc + Number(r.mar.replaceAll(',', ''))
-          }, 0)
-          .toLocaleString(),
-        apr: state.records
-          .reduce((acc, r) => {
-            return acc + Number(r.apr.replaceAll(',', ''))
-          }, 0)
-          .toLocaleString(),
-        may: state.records
-          .reduce((acc, r) => {
-            return acc + Number(r.may.replaceAll(',', ''))
-          }, 0)
-          .toLocaleString(),
-        jun: state.records
-          .reduce((acc, r) => {
-            return acc + Number(r.jun.replaceAll(',', ''))
-          }, 0)
-          .toLocaleString(),
-        jul: state.records
-          .reduce((acc, r) => {
-            return acc + Number(r.jul.replaceAll(',', ''))
-          }, 0)
-          .toLocaleString(),
-        aug: state.records
-          .reduce((acc, r) => {
-            return acc + Number(r.aug.replaceAll(',', ''))
-          }, 0)
-          .toLocaleString(),
-        sep: state.records
-          .reduce((acc, r) => {
-            return acc + Number(r.sep.replaceAll(',', ''))
-          }, 0)
-          .toLocaleString(),
-        oct: state.records
-          .reduce((acc, r) => {
-            return acc + Number(r.oct.replaceAll(',', ''))
-          }, 0)
-          .toLocaleString(),
-        nov: state.records
-          .reduce((acc, r) => {
-            return acc + Number(r.nov.replaceAll(',', ''))
-          }, 0)
-          .toLocaleString(),
-        dec: state.records
-          .reduce((acc, r) => {
-            return acc + Number(r.dec.replaceAll(',', ''))
-          }, 0)
-          .toLocaleString(),
-        cNameE: '合計',
-      })
-      console.log(state.records)
-    }
-    onMounted(() => {
-      setSubjects()
-      setClientsAndCosts()
-      state.selectClientsAndCostsName = state.clientsAndCosts[0].name
-      getFilteringData()
-    })
-
-    const sumPerMonth = (
-      list: { pay: number; month: number }[],
-      month: number
-    ) => {
-      return list
-        .filter((r) => r.month === month)
-        .reduce((sum: number, r) => {
-          return sum + r.pay
-        }, 0)
-        .toLocaleString()
-    }
-
     const columns = [
       {
         label: '',
@@ -419,16 +210,293 @@ export default defineComponent({
         sortable: false,
       },
     ]
+    const date = [
+      { id: '1', value: '01' },
+      { id: '2', value: '02' },
+      { id: '3', value: '03' },
+      { id: '4', value: '04' },
+      { id: '5', value: '05' },
+      { id: '6', value: '06' },
+      { id: '7', value: '07' },
+      { id: '8', value: '08' },
+      { id: '9', value: '09' },
+      { id: '10', value: '10' },
+      { id: '11', value: '11' },
+      { id: '12', value: '12' },
+      { id: '13', value: '13' },
+      { id: '14', value: '14' },
+      { id: '15', value: '15' },
+      { id: '16', value: '16' },
+      { id: '17', value: '17' },
+      { id: '18', value: '18' },
+      { id: '19', value: '19' },
+      { id: '20', value: '20' },
+      { id: '21', value: '21' },
+      { id: '22', value: '22' },
+      { id: '23', value: '23' },
+      { id: '24', value: '24' },
+      { id: '25', value: '25' },
+      { id: '26', value: '26' },
+      { id: '27', value: '27' },
+      { id: '28', value: '28' },
+      { id: '29', value: '29' },
+      { id: '30', value: '30' },
+      { id: '31', value: '31' },
+    ]
+
+    onMounted(() => {
+      // 勘定科目、取引先・固定経費をセット
+      setSubjects()
+      setClientsAndCosts()
+    })
+
+    /**
+     *
+     * 勘定科目変更時用イベント
+     *
+     */
     const changeSubject = () => {
-      state.clientsAndCosts.forEach((clients) => {
-        if (state.selectClientsAndCosts === clients.id) {
-          state.selectClientsAndCostsName = clients.name
-          console.log(state.selectClientsAndCostsName)
-        }
-      })
+      // 取引先・固定経費取得処理呼び出し
+      setClientsAndCosts()
+    }
+
+    /**
+     *
+     * 取引先・固定経費変更時用イベント
+     *
+     */
+    const changeClientsAndCosts = () => {
+      // 選択中の取引先・固定経費の名称をstateにセット
+      state.selectClientsAndCostsName = state.selectClientsAndCosts
+        .map((cLcO) => {
+          return cLcO.name
+        })
+        .join(',')
+
+      // 金額算出処理呼び出し
       getFilteringData()
     }
-    return { columns, changeSubject, ...toRefs(state) }
+
+    /**
+     *
+     * 勘定科目をstateにセットする
+     *
+     */
+    const setSubjects = () => {
+      const items = useState.subjectsInfo.value.reduce((ary: string[], g) => {
+        ary.push(g.groupname)
+        return ary
+      }, [])
+
+      // 最終結果をstateにセット
+      state.subjectItems = [...new Set(items)]
+    }
+
+    /**
+     *
+     * 取引先および固定経費をstateにセットする
+     *
+     */
+    const setClientsAndCosts = () => {
+      // 選択中の勘定科目コードを取得
+      const selectSubjectCode = useState.subjectsInfo.value.find((subject) => {
+        return subject.name === state.selectSubject
+      })?.id
+
+      if (selectSubjectCode) {
+        // 選択中の勘定科目コードを基に、取引データから取引先・固定経費コードをフィルタリングして取得
+        const usingClientsAndCosts = Array.from(
+          new Set(
+            useState.workRecordsManagement.value
+              .filter((workRecord) => {
+                return workRecord.sid === selectSubjectCode
+              })
+              .map((filterWorkRecord) => {
+                return filterWorkRecord.cid
+              })
+          )
+        )
+
+        // 取得した取引先・固定経費コードを含むデータからidとnameを抽出
+        const items = useState.clientsAndCostsInfo.value
+          .filter((info) => {
+            return usingClientsAndCosts.includes(info.id)
+          })
+          .map((filterInfo) => {
+            return { id: filterInfo.id, name: filterInfo.name }
+          })
+
+        // 最終結果をstateにセット
+        state.clientsAndCosts = [...new Set(items)]
+      } else {
+        // 勘定科目コードが取得できなかった場合、
+        // 取引先・固定経費を選択できないように空配列をセット
+        state.clientsAndCosts = []
+      }
+
+      // 選択中の取引先・固定経費をリセット
+      state.selectClientsAndCosts = []
+      state.selectClientsAndCostsName = ''
+    }
+
+    /**
+     *
+     * テーブル用のデータを作成しstateにセットする
+     *
+     */
+    const getFilteringData = () => {
+      // 月毎の合計金額算出用の関数を定義
+      const sumPerMonth = (
+        list: { pay: number; month: number }[],
+        month: number
+      ) => {
+        return list
+          .filter((r) => r.month === month)
+          .reduce((sum: number, r) => {
+            return sum + r.pay
+          }, 0)
+          .toLocaleString()
+      }
+
+      // stateを初期化
+      state.records.splice(0, state.records.length)
+
+      // ********************************************************************
+      // 指定の勘定科目、取引先・固定経費を使用している取引データを取得
+      // ********************************************************************
+      const filterRecordsManagements = useState.workRecordsManagement.value.filter(
+        (workRecord) => {
+          // 選択中の勘定科目グループ名を持つ勘定科目のIDを取得
+          const selectSubjectsId = useState.subjectsInfo.value
+            .filter((s) => {
+              return s.groupname === state.selectSubject
+            })
+            .map((fs) => {
+              return fs.id
+            })
+
+          // 選択中の取引先・固定経費情報からIDのみを取得
+          const selectClCo = state.selectClientsAndCosts.map((cLcO) => {
+            return cLcO.id
+          })
+
+          // 上記取得したIDを含む取引データのみを返却
+          return (
+            selectSubjectsId.includes(workRecord.sid) &&
+            selectClCo.includes(workRecord.cid)
+          )
+        }
+      )
+
+      // ※ 取引データが取得できなかった場合は後続処理は行わない
+      if (filterRecordsManagements.length > 0) {
+        // ********************************************************************
+        // 取引データの金額を日付毎に振り分け
+        // ********************************************************************
+        date.forEach((day) => {
+          // 取引データを「日」毎に確認
+          const filterByClientsAndCosts = filterRecordsManagements
+            .filter((records) => {
+              // 対象の「日」と同じ「日」を持つかでフィルタリング
+              return records.day.slice(8, 10) === day.value
+            })
+            .map((filterData) => {
+              // 取得した取引データから「金額」と「月」を抽出
+              return {
+                pay: filterData.pay,
+                month: Number(filterData.day.slice(5, 7)),
+              }
+            })
+
+          state.records.push({
+            cNameS: day.id + '日',
+            jan: sumPerMonth(filterByClientsAndCosts, 1),
+            feb: sumPerMonth(filterByClientsAndCosts, 2),
+            mar: sumPerMonth(filterByClientsAndCosts, 3),
+            apr: sumPerMonth(filterByClientsAndCosts, 4),
+            may: sumPerMonth(filterByClientsAndCosts, 5),
+            jun: sumPerMonth(filterByClientsAndCosts, 6),
+            jul: sumPerMonth(filterByClientsAndCosts, 7),
+            aug: sumPerMonth(filterByClientsAndCosts, 8),
+            sep: sumPerMonth(filterByClientsAndCosts, 9),
+            oct: sumPerMonth(filterByClientsAndCosts, 10),
+            nov: sumPerMonth(filterByClientsAndCosts, 11),
+            dec: sumPerMonth(filterByClientsAndCosts, 12),
+            cNameE: day.id + '日',
+          })
+        })
+
+        // ********************************************************************
+        // 合計行の算出
+        // ********************************************************************
+        state.records.push({
+          cNameS: '合計',
+          jan: state.records
+            .reduce((acc, r): number => {
+              return acc + Number(r.jan.replaceAll(',', ''))
+            }, 0)
+            .toLocaleString(),
+          feb: state.records
+            .reduce((acc, r) => {
+              return acc + Number(r.feb.replaceAll(',', ''))
+            }, 0)
+            .toLocaleString(),
+          mar: state.records
+            .reduce((acc, r) => {
+              return acc + Number(r.mar.replaceAll(',', ''))
+            }, 0)
+            .toLocaleString(),
+          apr: state.records
+            .reduce((acc, r) => {
+              return acc + Number(r.apr.replaceAll(',', ''))
+            }, 0)
+            .toLocaleString(),
+          may: state.records
+            .reduce((acc, r) => {
+              return acc + Number(r.may.replaceAll(',', ''))
+            }, 0)
+            .toLocaleString(),
+          jun: state.records
+            .reduce((acc, r) => {
+              return acc + Number(r.jun.replaceAll(',', ''))
+            }, 0)
+            .toLocaleString(),
+          jul: state.records
+            .reduce((acc, r) => {
+              return acc + Number(r.jul.replaceAll(',', ''))
+            }, 0)
+            .toLocaleString(),
+          aug: state.records
+            .reduce((acc, r) => {
+              return acc + Number(r.aug.replaceAll(',', ''))
+            }, 0)
+            .toLocaleString(),
+          sep: state.records
+            .reduce((acc, r) => {
+              return acc + Number(r.sep.replaceAll(',', ''))
+            }, 0)
+            .toLocaleString(),
+          oct: state.records
+            .reduce((acc, r) => {
+              return acc + Number(r.oct.replaceAll(',', ''))
+            }, 0)
+            .toLocaleString(),
+          nov: state.records
+            .reduce((acc, r) => {
+              return acc + Number(r.nov.replaceAll(',', ''))
+            }, 0)
+            .toLocaleString(),
+          dec: state.records
+            .reduce((acc, r) => {
+              return acc + Number(r.dec.replaceAll(',', ''))
+            }, 0)
+            .toLocaleString(),
+          cNameE: '合計',
+        })
+      }
+    }
+
+    return { ...toRefs(state), columns, changeSubject, changeClientsAndCosts }
   },
 })
 </script>
